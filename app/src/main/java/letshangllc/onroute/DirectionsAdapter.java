3c3320ce9c1859af72e5d7503df88cfa2aa942da
Carl Burnham <cburnham@letshang.us>
2015-12-22 09:16:37 -0800

    Added adapter for directions

---
+++ app/src/main/java/letshangllc/onroute/DirectionsAdapter.java
@@ -0,0 +1,50 @@
+package letshangllc.onroute;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Carl on 12/22/2015.
+ */
+public class DirectionsAdapter extends ArrayAdapter<Direction> {
+    private ArrayList<Direction> directions;
+
+    public DirectionsAdapter(Context context, ArrayList<Direction> directions){
+        super(context, R.layout.item_direction, directions);
+    }
+
+    private static class ViewHolder {
+        TextView tv_direction;
+    }
+
+    /*todo Set the direction of top position to bigger txt Size */
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // Get the data item for this position
+        Direction direction = getItem(position);
+        // Check if an existing view is being reused, otherwise inflate the view
+        ViewHolder viewHolder; // view lookup cache stored in tag
+
+        if (convertView == null) {
+            viewHolder = new ViewHolder();
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            convertView = inflater.inflate(R.layout.item_direction, parent, false);
+            viewHolder.tv_direction = (TextView) convertView.findViewById(R.id.tv_direction);
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+        // Populate the data into the template view using the data object
+        viewHolder.tv_direction.setText(direction.getDirection());
+        // Return the completed view to render on screen
+        return convertView;
+    }
+
+
+}
