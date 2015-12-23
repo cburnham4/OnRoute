894298ab29792b59000739bbdefc413248c80697
Carl Burnham <cburnham@letshang.us>
2015-12-23 13:30:49 -0800

    Added google places auto complete adapter

--- app/src/main/java/letshangllc/onroute/Activity_CreateRoute.java
+++ app/src/main/java/letshangllc/onroute/Activity_CreateRoute.java
@@ -1,10 +1,26 @@
 package letshangllc.onroute;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.places.Places;
+import com.google.android.gms.maps.SupportMapFragment;
+
+import java.util.ArrayList;
 
 /**
  * Created by Carl on 12/22/2015.
  */
 public class Activity_CreateRoute extends AppCompatActivity{
 
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_route);
+
+
+
+    }
 }
